75fa61c72846f380ca814892a692292c157be67f
Troy Hicks <NOLAWolfe@users.noreply.github.com>
2018-11-21 10:32:27 -0500

    test

--- WolfeWorldWide/WolfeWorldWide/WolfeWorldW.js
+++ WolfeWorldWide/WolfeWorldWide/WolfeWorldW.js
@@ -1,13 +1,14 @@
 window.onload = function(){
-    document.getElementById('getSchedule').addEventListener
-    ('click',getSchedule);
-    document.getElementById('getSchedule2').addEventListener
-    ('click',getSchedule2);
-    document.getElementById('getSchedule3').addEventListener
-    ('click',getSchedule3);
+    document.getElementById('getSchedule').addEventListener('click',getSchedule);
+    document.getElementById('getSchedule2').addEventListener('click',getSchedule2);
+    document.getElementById('getSchedule3').addEventListener('click',getSchedule3);
     
-    //document.getElementById('getF1Info').addEventListener
-    // ('click',getF1Info);
+    document.getElementById('getMovie').addEventListener('click',getMovie);
+}
+
+function getMovie(){
+    
+        
 }
 
 
@@ -54,10 +55,10 @@ window.onload = function(){
                         <div id="nas-li">
                             <table>
                                     <thead>
-                                        <th>${event.name}</th>
+                                        <th id="nas-li">${event.name}</th>
                                     </thead>
                                     <tbody>
-                                        <td>${event.start_date}</td>
+                                        <td id="nas-li">${event.start_date}</td>
                                     </tbody>
                             </table>
                         </div>
@@ -83,10 +84,10 @@ window.onload = function(){
                         <div id="nas-li">
                             <table>
                                     <thead>
-                                        <th>${event.name}</th>
+                                        <th id="nas-li">${event.name}</th>
                                     </thead>
                                     <tbody>
-                                        <td>${event.start_date}</td>
+                                        <td id="nas-li">${event.start_date}</td>
                                     </tbody>
                             </table>
                         </div>
@@ -99,36 +100,9 @@ window.onload = function(){
             
     }
 
+    
 
-    function getF1Info(){
-        var proxyUrl2 = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl2 = 'http://api.sportradar.us/formula1/trial/v2/en/competitors/sr:competitor:41127/profile.json?api_key=jgxy7nqef8ytrz3nb373ttyw';        
-        axios.get(proxyUrl2 + targetUrl2)
-            .then((response) => {
-                let schedule = response.data.events;
-                let output = '';
-                $.each(schedule,(index,event)=>{
-                    console.log(event);
-                    output+=`
-                        <div id="nas-li">
-                            <table>
-                                    <thead>
-                                        <th>${event.team}</th>
-                                    </thead>
-                                    <tbody>
-                                        <td>${event.nationality}</td>
-                                        <td>${event.nationality}</td>
-                                    </tbody>
-                            </table>
-                        </div>
-                    `;
-                })
-                console.log(response);
-                $('#output').html(output);
-            })
 
-            
-    }
 //     <script>
 // var txt = '{"name":"John", "age":30, "city":"New York"}'
 // var obj = JSON.parse(txt);
